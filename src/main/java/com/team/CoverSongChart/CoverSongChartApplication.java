4313d35b6b557539d2f39a82756b15dacb0236cf
CycoTest <aceldamadk@gmail.com>
2023-09-09 17:22:58 +0900

    스프링부트 프로젝트 초기 커밋

---
+++ src/main/java/com/team/CoverSongChart/CoverSongChartApplication.java
@@ -0,0 +1,13 @@
+package com.team.CoverSongChart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoverSongChartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoverSongChartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/CoverSongChart/CoverSongChartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.CoverSongChart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoverSongChartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
