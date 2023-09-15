a0640fcee2d7e38be6de3b028e5585ac5d691c2b
CycoTest <aceldamadk@gmail.com>
2023-09-15 19:26:57 +0900

    ver.1 - WebApp Front-end proto version making

--- src/main/resources/static/javaScript/home.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector(".menu-icon");
-const sideBar = document.querySelector(".sideBar");
-
-menuIcon.onclick = function () {
-    sideBar.classList.toggle("sidebar-left");
-}
