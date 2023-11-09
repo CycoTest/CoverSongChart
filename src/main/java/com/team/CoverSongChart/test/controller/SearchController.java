c9a7cbe651c7b2377ffc916f97741e14af9bf34d
Hoonyyyy <hyunsoo34242686@gmail.com>
2023-11-09 23:24:04 +0900

    backApi-youtube-search playlist, playlistInfo, videoInfo
    backApi-youtube-search playlist, playlistInfo, videoInfo

--- src/main/java/com/team/CoverSongChart/test/controller/SearchController.java
+++ src/main/java/com/team/CoverSongChart/test/controller/SearchController.java
@@ -2,13 +2,12 @@ package com.team.CoverSongChart.test.controller;
 
 import com.google.api.services.youtube.model.Playlist;
 import com.google.api.services.youtube.model.PlaylistItem;
+import com.google.api.services.youtube.model.PlaylistItemListResponse;
+import com.google.api.services.youtube.model.Video;
 import com.team.CoverSongChart.test.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -35,30 +34,43 @@ public class SearchController {
         return searchService.getPopularVideo();
     }
 
+
+
+
     // 플레이리스트 가져오기
+    @GetMapping("/playlist")
+    public Playlist getPlaylistById(@RequestParam String playlistId, Model model) throws IOException {
+        return searchService.getPlaylist(playlistId);
+    }
 
     @GetMapping("/playlistInfo")
-    public String getPlaylistInfo(@RequestParam("playlistId") String playlistId, Model model) {
-        Playlist playlist = searchService.getPlaylistInfo(playlistId);
-        if (playlist != null) {
-            model.addAttribute("playlist", playlist);
-            return searchService.getPlaylistInfo(playlistId).toString();
+    public PlaylistItemListResponse getPlaylistInfoInVideo(@RequestParam String playlistId) throws IOException {
+        return searchService.getPlaylistInfoInVideo(playlistId);
+    }
+
+    @GetMapping("/videoInfo")
+    public String getVideoInfo(@RequestParam("videoId") String videoId, Model model) {
+        Video video = searchService.getVideoInfo(videoId);
+        if (video != null) {
+            model.addAttribute("video", video);
+            return searchService.getVideoInfo(videoId).toString();
         } else {
             return "error"; // 에러 페이지로 리다이렉트 또는 표시
         }
     }
 
 
-    @GetMapping("/playlistInfoDetail")
-    public String getPlaylistInfoDetail(@RequestParam("playlistId") String playlistId, Model model) {
-        List<String> videoList = searchService.getVideosInPlaylistDetail(playlistId);
 
-        if (!videoList.isEmpty()) {
-            model.addAttribute("videoList", videoList); // 동영상 목록을 모델에 추가
-            return searchService.getVideosInPlaylistDetail(playlistId).toString();
-        } else {
-            return "error"; // 에러 페이지로 리다이렉트 또는 표시
-        }
-    }
+//    @GetMapping("/playlistInfoDetail")
+//    public String getPlaylistInfoDetail(@RequestParam("playlistId") String playlistId, Model model) {
+//        List<String> videoList = searchService.getVideosInPlaylistDetail(playlistId);
+//
+//        if (!videoList.isEmpty()) {
+//            model.addAttribute("videoList", videoList); // 동영상 목록을 모델에 추가
+//            return searchService.getVideosInPlaylistDetail(playlistId).toString();
+//        } else {
+//            return "error"; // 에러 페이지로 리다이렉트 또는 표시
+//        }
+//    }
 }
 
--- src/main/java/com/team/CoverSongChart/test/service/SearchService.java
+++ src/main/java/com/team/CoverSongChart/test/service/SearchService.java
@@ -6,8 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 
@@ -21,37 +19,25 @@ public class SearchService {
         YouTube.Search.List search = youtube.search().list("snippet");
 
         String searchQuery = keyword + "cover";
-
         search.setQ(searchQuery);
         search.setType("video"); // 동영상 결과만 필터링
         search.setVideoCategoryId("10"); // "음악" 카테고리 ID 설정
-
-        // if-else
-        // 음악으로 두고
-        // 그 다음에는 cover, cover song, 커버
-
         search.setMaxResults(5L); // 검색제한 갯수 원하면 하기
 
         SearchListResponse searchResponse = search.execute();
         return searchResponse.toPrettyString();
-
-        //GSON 이용
-//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//        String prettyJson = gson.toJson(gson);
-
-//        return prettyJson;
     }
 
     public List<PlaylistItem> getPlaylistItems(String playlistId) throws IOException {
         YouTube.PlaylistItems.List request = youtube.playlistItems().list("snippet");
         request.setPlaylistId(playlistId);
         request.setMaxResults(10L);
-
         PlaylistItemListResponse playlistItemListResponse = request.execute();
         return playlistItemListResponse.getItems();
     }
 
 
+    // 한국 최근 인기영상들 가져오기
     public String getPopularVideo() throws IOException {
         YouTube.Videos.List videoRequest = youtube.videos().list("snippet");
         videoRequest.setRegionCode("KR"); // 한국 지역 코드
@@ -63,15 +49,36 @@ public class SearchService {
     }
 
 
-    // 내꺼 playlist 가져오기 23.11
+    ///////////////
 
-    public Playlist getPlaylistInfo(String playlistId) {
+    // playlist 겉 표지
+    // http://localhost:8080/csc/playlist?playlistId=PLOlzssqbfJZhUp5FFVHYaepkba__DfMTC
+    public Playlist getPlaylist(String playlistId) throws IOException {
+        return youtube.playlists().list("snippet")
+                .setId(playlistId)
+                .execute()
+                .getItems()
+                .get(0);
+    }
+
+    // playlist 안에 담긴 videoId에 대한 정보만 가져오기
+    // 아래 예시
+    // http://localhost:8080/csc/playlistInfo?playlistId=PLOlzssqbfJZhUp5FFVHYaepkba__DfMTC
+    public PlaylistItemListResponse getPlaylistInfoInVideo(String playlistId) throws IOException {
+        return youtube.playlistItems().list("snippet")
+                .setPlaylistId(playlistId)
+                .execute();
+    }
+
+    // VideoId에 대한 것 가져오기
+    // 아래 예시
+    // http://localhost:8080/csc/videoInfo?videoId=3gn31nL_UVc
+    public Video getVideoInfo(String videoId) {
         try {
-            // 플레이리스트 정보 가져오기
-            YouTube.Playlists.List playlistsListRequest = youtube.playlists().list("snippet");
-            playlistsListRequest.setId(playlistId); // 플레이리스트 ID 입력
-            PlaylistListResponse playlistsListResponse = playlistsListRequest.execute();
-            return playlistsListResponse.getItems().get(0);
+            YouTube.Videos.List videosListRequest = youtube.videos().list("snippet");
+            videosListRequest.setId(videoId);
+            VideoListResponse videoListResponse = videosListRequest.execute();
+            return videoListResponse.getItems().get(0);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
@@ -79,20 +86,35 @@ public class SearchService {
     }
 
 
-    public List<String> getVideosInPlaylistDetail(String playlistId) {
-        try {
-            YouTube.PlaylistItems.List playlistItemsRequest = youtube.playlistItems().list("snippet");
-            playlistItemsRequest.setPlaylistId(playlistId);
-            PlaylistItemListResponse playlistItemListResponse = playlistItemsRequest.execute();
+    // 내꺼 playlist 가져오기 23.11
+//    public Playlist getPlaylistInfo(String playlistId) {
+//        try {
+//            // 플레이리스트 정보 가져오기
+//            YouTube.Playlists.List playlistsListRequest = youtube.playlists().list("snippet");
+//            playlistsListRequest.setId(playlistId); // 플레이리스트 ID 입력
+//            PlaylistListResponse playlistsListResponse = playlistsListRequest.execute();
+//            return playlistsListResponse.getItems().get(0);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
 
-            List<String> videoList = new ArrayList<>();
-            for (PlaylistItem playlistItem : playlistItemListResponse.getItems())
-                videoList.add(playlistItem.getSnippet().getResourceId().getVideoId());
+    //    public List<String> getVideosInPlaylistDetail(String playlistId) {
+//        try {
+//            YouTube.PlaylistItems.List playlistItemsRequest = youtube.playlistItems().list("snippet");
+//            playlistItemsRequest.setPlaylistId(playlistId);
+//            PlaylistItemListResponse playlistItemListResponse = playlistItemsRequest.execute();
+//
+//            List<String> videoList = new ArrayList<>();
+//            for (PlaylistItem playlistItem : playlistItemListResponse.getItems())
+//                videoList.add(playlistItem.getSnippet().getResourceId().getVideoId());
+//
+//            return videoList;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return Collections.emptyList();
+//        }
+//    }
 
-            return videoList;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return Collections.emptyList();
-        }
-    }
 }
