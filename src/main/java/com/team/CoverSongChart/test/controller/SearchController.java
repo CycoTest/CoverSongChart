e44e240e60af94bf09f611d3c7bf3260489856e4
Hoonyyyy <hyunsoo34242686@gmail.com>
2023-12-03 13:13:05 +0900

    SearchController commit
    playList Read function start

--- src/main/java/com/team/CoverSongChart/test/controller/SearchController.java
+++ src/main/java/com/team/CoverSongChart/test/controller/SearchController.java
@@ -1,9 +1,9 @@
 package com.team.CoverSongChart.test.controller;
 
 import com.google.api.services.youtube.model.Playlist;
-import com.google.api.services.youtube.model.PlaylistItem;
 import com.google.api.services.youtube.model.PlaylistItemListResponse;
 import com.google.api.services.youtube.model.Video;
+import com.team.CoverSongChart.test.DTO.playlistInfoDTO.A_PlaylistItemDTO;
 import com.team.CoverSongChart.test.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
-@RestController
 @RequestMapping("/csc")
 @RequiredArgsConstructor
+@RestController
 public class SearchController {
 
     private final SearchService searchService;
@@ -24,30 +24,21 @@ public class SearchController {
         return searchService.searchAll(keyword);
     }
 
-    @GetMapping("/music/list")
-    public List<PlaylistItem> getPlaylistItems(@RequestParam String keyword) throws IOException {
-        return searchService.getPlaylistItems(keyword);
-    }
-
-    @GetMapping("/popular")
-    public String popularVideo() throws IOException {
-        return searchService.getPopularVideo();
-    }
-
-
-
-
-    // 플레이리스트 가져오기
+    // 플레이리스트 가져오기 (겉 표지)
+    //
     @GetMapping("/playlist")
     public Playlist getPlaylistById(@RequestParam String playlistId, Model model) throws IOException {
         return searchService.getPlaylist(playlistId);
     }
 
+    // dto 이용 playlist 안에 내용들 가져오기
+    // http://localhost:8080/csc/playlistInfo?playlistId=PLOlzssqbfJZhUp5FFVHYaepkba__DfMTC&continue
     @GetMapping("/playlistInfo")
-    public PlaylistItemListResponse getPlaylistInfoInVideo(@RequestParam String playlistId) throws IOException {
+    public A_PlaylistItemDTO getPlaylistInfoInVideo(@RequestParam String playlistId) throws IOException {
         return searchService.getPlaylistInfoInVideo(playlistId);
     }
 
+    // video 1개에 담긴 내용
     @GetMapping("/videoInfo")
     public String getVideoInfo(@RequestParam("videoId") String videoId, Model model) {
         Video video = searchService.getVideoInfo(videoId);
@@ -58,8 +49,26 @@ public class SearchController {
             return "error"; // 에러 페이지로 리다이렉트 또는 표시
         }
     }
+}
 
 
+
+
+//    @GetMapping("/playlistInfo")
+//    public PlaylistItemListResponse getPlaylistInfoInVideo(@RequestParam String playlistId) throws IOException {
+//        return searchService.getPlaylistInfoInVideo(playlistId);
+//    }
+
+
+    //    @GetMapping("/music/list")
+//    public List<PlaylistItem> getPlaylistItems(@RequestParam String keyword) throws IOException {
+//        return searchService.getPlaylistItems(keyword);
+//    }
+//
+//    @GetMapping("/popular")
+//    public String popularVideo() throws IOException {
+//        return searchService.getPopularVideo();
+//    }
 
 //    @GetMapping("/playlistInfoDetail")
 //    public String getPlaylistInfoDetail(@RequestParam("playlistId") String playlistId, Model model) {
@@ -72,5 +81,5 @@ public class SearchController {
 //            return "error"; // 에러 페이지로 리다이렉트 또는 표시
 //        }
 //    }
-}
+
 
